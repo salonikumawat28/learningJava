430ef6140fb0cbdb46dfa7afaaee4d23a807043b
salonikumawat28 <545saloni@gmail.com>
2020-10-30 23:30:44 +0530

    Adding a TODO

--- assignments/RunningSum.java
+++ assignments/RunningSum.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 public class RunningSum {
     
     public static void main(String[] args) {
+        // TODO: Hard Code the input instead from taking from terminal.
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter the Length of the Array");
